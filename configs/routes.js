e3c8a4a1169ad80c4154c54137d62fffefcfa5c3
vincenttian <vincenttian16@gmail.com>
2014-06-20 16:57:12 -0700

    take out register code; begin integrating passport login with linkedin account

--- configs/routes.js
+++ configs/routes.js
@@ -1,60 +1,39 @@
 var Linkedin = require('node-linkedin')('452p27539u5f', '3q1iiaeQph2wRH4M', 'http://localhost:3000/oauth/linkedin/callback');
 var linkedin;
 
-// OLD AUTHENTICATION
-
-// module.exports = function(app) {
-
-//     // Unauthenticated Index page
-//     app.get('/', function(req, res) {
-//         console.log(req.session);
-//         res.render('index_unauthenticated.ejs', {
-//             title: 'LinkedIn Tinder'
-//         });
-//     });
-
-//     // Authenticated Index page
-//     app.get('/index', function(req, res) {
-//         console.log(req.session);
-//         // Query for more atributes here to load in database
-//         linkedin.people.me(['id', 'first-name', 'last-name'], function(err, $in) {
-//             res.render('index.ejs', {
-//                 title: 'LinkedIn Tinder',
-//                 username: $in['firstName']
-//             });
-//         });
-//     });
-
-//     app.get('/data', function(req, res) {
-//         console.log(req.session);
-//         linkedin.people.me(function(err, $in) {
-//             if (err) return err;
-//             res.json({
-//                 profile: $in
-//             })
-//         });
-//     });
-
-//     app.get('/oauth/linkedin', function(req, res) {
-//         Linkedin.auth.authorize(res, ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups']);
-//     });
-
-//     app.get('/oauth/linkedin/callback', function(req, res) {
-//         Linkedin.auth.getAccessToken(res, req.query.code, function(err, results) {
-//             if (err) return console.error(err);
-//             results = JSON.parse(results);
-//             linkedin = Linkedin.init(results["access_token"]);
-//             return res.redirect('/index');
-//         });
-//     });
-// }
-
-
 // BEGIN PASSPORT TUTORIAL SIGNUP
 
-// app/routes.js
 module.exports = function(app, passport) {
     var passport = require('passport');
+
+    app.get('/oauth/linkedin', function(req, res) {
+        Linkedin.auth.authorize(res, ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups']);
+    });
+
+    app.get('/oauth/linkedin/callback', function(req, res) {
+        Linkedin.auth.getAccessToken(res, req.query.code, function(err, results) {
+            if (err) return console.error(err);
+            results = JSON.parse(results);
+            linkedin = Linkedin.init(results["access_token"]);
+            return res.redirect('/signup');
+            // create user here and store in database
+
+            // res.render('profile.ejs', {
+            //     user: req.user // get the user out of session and pass to template
+            // });
+        });
+    });
+
+    app.get('/data', function(req, res) {
+        console.log(req.session);
+        linkedin.people.me(function(err, $in) {
+            if (err) return err;
+            res.json({
+                profile: $in
+            })
+        });
+    });
+
 
     // =====================================
     // HOME PAGE (with login links) ========
